dc7ca1fc40bc36d4d44aa50d4b017788552327ce
Andreas Mantke <maand@gmx.de>
2012-05-19 20:52:29 +0200

    roomfolder and room object added and some changes on the templates and files to make the add-on more usable and to reference objects

---
+++ src/collective/conference/room.py
@@ -0,0 +1,51 @@
+from five import grok
+from zope import schema
+
+from plone.directives import form, dexterity
+
+from plone.app.textfield import RichText
+from plone.namedfile.field import NamedImage
+
+from zope.app.container.interfaces import IObjectAddedEvent
+from Products.CMFCore.utils import getToolByName
+
+from collective.conference import _
+
+class IRoom(form.Schema):
+    """A conference room.
+    """
+    
+    title = schema.TextLine(
+            title=_(u"Name of the Room"),
+        )
+
+
+    description = schema.Text(
+            title=_(u"A description of the room and its location"),
+        )
+
+    
+    picture = NamedImage(
+            title=_(u"A picture of the room"),
+            description=_(u"Please upload an image"),
+            required=False,
+        )
+    
+    form.primary ('details')
+    details = RichText(
+             title=_(u"A full description of the room, it's location and the way to get there"),
+             required=False                          
+        )
+    
+    capacity = schema.Int(
+             title=_(u"Capacity of the room"),
+             description=_(u"Please fill in the maximum number of attendees"),
+             required=False,
+        )
+
+@grok.subscribe(IRoom, IObjectAddedEvent)
+
+
+class View(grok.View):
+    grok.context(IRoom)
+    grok.require('zope2.View')
---
+++ src/collective/conference/roomfolder.py
@@ -0,0 +1,46 @@
+from five import grok
+from zope import schema
+
+from zope.component import createObject
+from zope.event import notify
+from zope.lifecycleevent import ObjectCreatedEvent
+from zope.filerepresentation.interfaces import IFileFactory
+
+from DateTime import DateTime
+from plone.indexer import indexer
+
+from plone.directives import form
+from plone.app.textfield import RichText
+
+from plone.formwidget.autocomplete import AutocompleteFieldWidget
+from z3c.form.browser.textlines import TextLinesFieldWidget
+
+from collective.conference import _
+
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
+
+from plone.directives import form, dexterity
+from zope.security import checkPermission
+
+from collective.conference.room import IRoom
+
+class IRoomfolder(form.Schema):
+    """A folder for the rooms of a conference.
+    """
+
+
+    title = schema.TextLine(
+            title=_(u"Name of the room folder"),
+        )
+
+    description = schema.Text(
+            title=_(u"roomfolder description"),
+        )
+    
+class View(dexterity.DisplayForm):
+    grok.context(IRoomfolder)
+    grok.require('zope2.View')
+
+    def canRequestReview(self):
+        return checkPermission('cmf.RequestReview', self.context)   
